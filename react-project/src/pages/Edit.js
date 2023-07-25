b82aa6f922bb40385196928731f24db0f331634e
Hasdiseh <133518815+HadisehArmand@users.noreply.github.com>
2023-07-26 02:05:10 +0330

    Start of changes

--- react-project/src/pages/Edit.js
+++ react-project/src/pages/Edit.js
@@ -2,51 +2,59 @@ import React from "react";
 import axios from "axios";
 import SetAction from "../addLogsys";
 import getUser from "../getUser";
-function Edit (){
+class Edit extends React.Component {
   constructor(props) {
     super(props);
-    let users = JSON.parse(localStorage.getItem('users'))
+    let users = JSON.parse(localStorage.getItem("users"));
     for (let index = 0; index < users.length; index++) {
-      if (parseInt(users[index]["id"])===parseInt(props.id)) {
-        users = users[index]
+      if (parseInt(users[index]["id"]) === parseInt(props.id)) {
+        users = users[index];
       }
-      
     }
     this.state = {
-      id:users['id'],
-      firstname: users['firstname'],
-      lastname: users['lastname'],
-      email: users['email'],
-      password: users['password'],
-      avatar: users['avatar'],
-    }
-    this.submitUser = this.submitUser.bind(this)
+      id: users["id"],
+      firstname: users["firstname"],
+      lastname: users["lastname"],
+      email: users["email"],
+      password: users["password"],
+      avatar: users["avatar"],
+    };
+    this.submitUser = this.submitUser.bind(this);
   }
   submitUser() {
-    if (this.state.firstname !== '' && this.state.lastname !== '' && this.state.email !== '' && this.state.password !== '' && this.state.avatar !== '') {
+    if (
+      this.state.firstname !== "" &&
+      this.state.lastname !== "" &&
+      this.state.email !== "" &&
+      this.state.password !== "" &&
+      this.state.avatar !== ""
+    ) {
       let bodyUser = JSON.stringify(this.state);
-      let baseUrl = 'https://64b53279f3dbab5a95c6e9e2.mockapi.io/api/v1/user/'+this.props.id
-      axios.put(`${baseUrl}`, bodyUser, {
-        headers: {
-          'content-type': 'application/json',
-        },
-      })
-        .then(res => {
-          alert(JSON.stringify(res.data) + "\nEdited !")
-          SetAction(`"Edit login with id ${this.props.id}"`,"ok")
-          getUser()
-          window.location.href = "/view"
+      let baseUrl =
+        "https://64b53279f3dbab5a95c6e9e2.mockapi.io/api/v1/user/" +
+        this.props.id;
+      axios
+        .put(`${baseUrl}`, bodyUser, {
+          headers: {
+            "content-type": "application/json",
+          },
         })
+        .then((res) => {
+          alert(JSON.stringify(res.data) + "\nEdited !");
+          SetAction(`"Edit login with id ${this.props.id}"`, "ok");
+          getUser();
+          window.location.href = "/view";
+        });
     } else {
-      alert("plaese complite form !")
-      SetAction(`"Edit login with id ${this.props.id}"`,"False")
+      alert("plaese complite form !");
+      SetAction(`"Edit login with id ${this.props.id}"`, "False");
     }
   }
   updateInputValue(evt) {
     const val = evt.target.value;
     const idV = evt.target.id;
     this.setState({
-      [idV]: val
+      [idV]: val,
     });
   }
   render() {
@@ -56,7 +64,9 @@ function Edit (){
           <div className="row d-flex justify-content-center">
             <h1 className="text-center">Edit</h1>
             <div className="input-group mb-3  d-flex justify-content-center">
-              <span className="input-group-text" id="basic-addon1">F</span>
+              <span className="input-group-text" id="basic-addon1">
+                F
+              </span>
               <input
                 id="firstname"
                 type="text"
@@ -65,13 +75,15 @@ function Edit (){
                 aria-label="FirstName"
                 aria-describedby="basic-addon1"
                 value={this.state.firstname}
-                onChange={evt => this.updateInputValue(evt)}
+                onChange={(evt) => this.updateInputValue(evt)}
               />
             </div>
           </div>
           <div className="row d-flex justify-content-center">
             <div className="input-group mb-3">
-              <span className="input-group-text" id="basic-addon1">L</span>
+              <span className="input-group-text" id="basic-addon1">
+                L
+              </span>
               <input
                 id="lastname"
                 type="text"
@@ -80,13 +92,15 @@ function Edit (){
                 aria-label="LastName"
                 aria-describedby="basic-addon1"
                 value={this.state.lastname}
-                onChange={evt => this.updateInputValue(evt)}
+                onChange={(evt) => this.updateInputValue(evt)}
               />
             </div>
           </div>
           <div className="row d-flex justify-content-center">
             <div className="input-group mb-3">
-              <span className="input-group-text" id="basic-addon1">@</span>
+              <span className="input-group-text" id="basic-addon1">
+                @
+              </span>
               <input
                 id="email"
                 type="email"
@@ -95,13 +109,15 @@ function Edit (){
                 aria-label="email"
                 aria-describedby="basic-addon1"
                 value={this.state.email}
-                onChange={evt => this.updateInputValue(evt)}
+                onChange={(evt) => this.updateInputValue(evt)}
               />
             </div>
           </div>
           <div className="row d-flex justify-content-center">
             <div className="input-group mb-3">
-              <span className="input-group-text" id="basic-addon1">P</span>
+              <span className="input-group-text" id="basic-addon1">
+                P
+              </span>
               <input
                 id="password"
                 type="password"
@@ -110,13 +126,15 @@ function Edit (){
                 aria-label="password"
                 aria-describedby="basic-addon1"
                 value={this.state.password}
-                onChange={evt => this.updateInputValue(evt)}
+                onChange={(evt) => this.updateInputValue(evt)}
               />
             </div>
           </div>
           <div className="row d-flex justify-content-center">
             <div className="input-group mb-3">
-              <span className="input-group-text" id="basic-addon1">A</span>
+              <span className="input-group-text" id="basic-addon1">
+                A
+              </span>
               <input
                 id="avatar"
                 type="text"
@@ -125,13 +143,18 @@ function Edit (){
                 aria-label="Avatar"
                 aria-describedby="basic-addon1"
                 value={this.state.avatar}
-                onChange={evt => this.updateInputValue(evt)}
+                onChange={(evt) => this.updateInputValue(evt)}
               />
             </div>
           </div>
           <div className="row d-flex justify-content-center">
             <div className="col-6 d-flex justify-content-evenly">
-              <input className="btn" type="button" value="Edit" onClick={this.submitUser} />
+              <input
+                className="btn"
+                type="button"
+                value="Edit"
+                onClick={this.submitUser}
+              />
             </div>
           </div>
         </div>
