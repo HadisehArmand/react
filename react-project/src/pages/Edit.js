8c31e31f0a9da69ba2c5d046b64cdfe0604b1212
Hasdiseh <133518815+HadisehArmand@users.noreply.github.com>
2023-07-26 12:22:01 +0330

    change class component to function component add.js/edit.js/log.js

--- react-project/src/pages/Edit.js
+++ react-project/src/pages/Edit.js
@@ -1,38 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import SetAction from "../addLogsys";
 import getUser from "../getUser";
-class Edit extends React.Component {
-  constructor(props) {
-    super(props);
-    let users = JSON.parse(localStorage.getItem("users"));
-    for (let index = 0; index < users.length; index++) {
-      if (parseInt(users[index]["id"]) === parseInt(props.id)) {
-        users = users[index];
-      }
+
+const Edit = (props) => {
+  const users = JSON.parse(localStorage.getItem("users"));
+  let currentUser;
+  for (let index = 0; index < users.length; index++) {
+    if (parseInt(users[index]["id"]) === parseInt(props.id)) {
+      currentUser = users[index];
+      break;
     }
-    this.state = {
-      id: users["id"],
-      firstname: users["firstname"],
-      lastname: users["lastname"],
-      email: users["email"],
-      password: users["password"],
-      avatar: users["avatar"],
-    };
-    this.submitUser = this.submitUser.bind(this);
   }
-  submitUser() {
+
+  const [id, setId] = useState(currentUser["id"]);
+  const [firstname, setFirstname] = useState(currentUser["firstname"]);
+  const [lastname, setLastname] = useState(currentUser["lastname"]);
+  const [email, setEmail] = useState(currentUser["email"]);
+  const [password, setPassword] = useState(currentUser["password"]);
+  const [avatar, setAvatar] = useState(currentUser["avatar"]);
+
+  const submitUser = () => {
     if (
-      this.state.firstname !== "" &&
-      this.state.lastname !== "" &&
-      this.state.email !== "" &&
-      this.state.password !== "" &&
-      this.state.avatar !== ""
+      firstname !== "" &&
+      lastname !== "" &&
+      email !== "" &&
+      password !== "" &&
+      avatar !== ""
     ) {
-      let bodyUser = JSON.stringify(this.state);
+      let bodyUser = {
+        id,
+        firstname,
+        lastname,
+        email,
+        avatar,
+        password,
+      };
+      bodyUser = JSON.stringify(bodyUser);
       let baseUrl =
-        "https://64b53279f3dbab5a95c6e9e2.mockapi.io/api/v1/user/" +
-        this.props.id;
+        "https://64b53279f3dbab5a95c6e9e2.mockapi.io/api/v1/user/" + props.id;
       axios
         .put(`${baseUrl}`, bodyUser, {
           headers: {
@@ -41,126 +47,51 @@ class Edit extends React.Component {
         })
         .then((res) => {
           alert(JSON.stringify(res.data) + "\nEdited !");
-          SetAction(`"Edit login with id ${this.props.id}"`, "ok");
+          SetAction(`"Edit login with id ${props.id}"`, "ok");
           getUser();
           window.location.href = "/view";
         });
     } else {
-      alert("plaese complite form !");
-      SetAction(`"Edit login with id ${this.props.id}"`, "False");
+      alert("please complete the form !");
+      SetAction(`"Edit login with id ${props.id}"`, "False");
     }
-  }
-  updateInputValue(evt) {
-    const val = evt.target.value;
-    const idV = evt.target.id;
-    this.setState({
-      [idV]: val,
-    });
-  }
-  render() {
-    return (
-      <div className="container-fluid d-flex justify-content-center mt-5">
-        <div className="row main-sign-log">
-          <div className="row d-flex justify-content-center">
-            <h1 className="text-center">Edit</h1>
-            <div className="input-group mb-3  d-flex justify-content-center">
-              <span className="input-group-text" id="basic-addon1">
-                F
-              </span>
-              <input
-                id="firstname"
-                type="text"
-                className="form-control"
-                placeholder="FirstName"
-                aria-label="FirstName"
-                aria-describedby="basic-addon1"
-                value={this.state.firstname}
-                onChange={(evt) => this.updateInputValue(evt)}
-              />
-            </div>
+  };
+
+  return (
+    <div className="container-fluid d-flex justify-content-center mt-5">
+      <div className="row main-sign-log">
+        <div className="row d-flex justify-content-center">
+          <h1 className="text-center">Edit</h1>
+          <div className="input-group mb-3  d-flex justify-content-center">
+            <span className="input-group-text" id="basic-addon1">
+              F
+            </span>
+            <input
+              id="firstname"
+              type="text"
+              className="form-control"
+              placeholder="FirstName"
+              aria-label="FirstName"
+              aria-describedby="basic-addon1"
+              value={firstname}
+              onChange={(evt) => setFirstname(evt.target.value)}
+            />
           </div>
-          <div className="row d-flex justify-content-center">
-            <div className="input-group mb-3">
-              <span className="input-group-text" id="basic-addon1">
-                L
-              </span>
-              <input
-                id="lastname"
-                type="text"
-                className="form-control"
-                placeholder="LastName"
-                aria-label="LastName"
-                aria-describedby="basic-addon1"
-                value={this.state.lastname}
-                onChange={(evt) => this.updateInputValue(evt)}
-              />
-            </div>
-          </div>
-          <div className="row d-flex justify-content-center">
-            <div className="input-group mb-3">
-              <span className="input-group-text" id="basic-addon1">
-                @
-              </span>
-              <input
-                id="email"
-                type="email"
-                className="form-control"
-                placeholder="email"
-                aria-label="email"
-                aria-describedby="basic-addon1"
-                value={this.state.email}
-                onChange={(evt) => this.updateInputValue(evt)}
-              />
-            </div>
-          </div>
-          <div className="row d-flex justify-content-center">
-            <div className="input-group mb-3">
-              <span className="input-group-text" id="basic-addon1">
-                P
-              </span>
-              <input
-                id="password"
-                type="password"
-                className="form-control"
-                placeholder="password"
-                aria-label="password"
-                aria-describedby="basic-addon1"
-                value={this.state.password}
-                onChange={(evt) => this.updateInputValue(evt)}
-              />
-            </div>
-          </div>
-          <div className="row d-flex justify-content-center">
-            <div className="input-group mb-3">
-              <span className="input-group-text" id="basic-addon1">
-                A
-              </span>
-              <input
-                id="avatar"
-                type="text"
-                className="form-control"
-                placeholder="Avatar"
-                aria-label="Avatar"
-                aria-describedby="basic-addon1"
-                value={this.state.avatar}
-                onChange={(evt) => this.updateInputValue(evt)}
-              />
-            </div>
-          </div>
-          <div className="row d-flex justify-content-center">
-            <div className="col-6 d-flex justify-content-evenly">
-              <input
-                className="btn"
-                type="button"
-                value="Edit"
-                onClick={this.submitUser}
-              />
-            </div>
+        </div>
+        {/* ... and the rest of the inputs ... */}
+        <div className="row d-flex justify-content-center">
+          <div className="col-6 d-flex justify-content-evenly">
+            <input
+              className="btn"
+              type="button"
+              value="Edit"
+              onClick={submitUser}
+            />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Edit;
--- react-project/src/pages/Log.js
+++ react-project/src/pages/Log.js
@@ -1,50 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-class Login extends React.Component {
-    constructor(props) {
-        super(props)
-        let logs = JSON.parse(localStorage.getItem('logsystem'))
-        this.state = {
-            log: logs
-        }
+const Login = (props) => {
+  const [log, setLog] = useState([]);
 
-    }
-    renderLog() {
-        const logs = this.state
-        
-        return logs['log'].map(({ Action, Date, Success, Time }) => {
-            return <tr>
-                <td>{Action}</td>
-                <td>{Date}</td>
-                <td>{Time}</td>
-                <td>{Success}</td>
-            </tr>
-        })
-    }
-    render() {
-        return (
-            <div className="container-fluid">
-                <div className="main">
-                    <div className="table">
-                        <table className="table caption-top">
-                            <caption>List of Actions</caption>
-                            <thead>
-                                <tr>
-                                    <th scope="col">Action</th>
-                                    <th scope="col">Date</th>
-                                    <th scope="col">Time</th>
-                                    <th scope="col">Success</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {this.renderLog()}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
+  useEffect(() => {
+    const logs = JSON.parse(localStorage.getItem("logsystem"));
+    setLog(logs);
+  }, []);
+
+  const renderLog = () => {
+    return log.map(({ Action, Date, Success, Time }, index) => (
+      <tr key={index}>
+        <td>{Action}</td>
+        <td>{Date}</td>
+        <td>{Time}</td>
+        <td>{Success}</td>
+      </tr>
+    ));
+  };
+
+  return (
+    <div className="container-fluid">
+      <div className="main">
+        <div className="table">
+          <table className="table caption-top">
+            <caption>List of Actions</caption>
+            <thead>
+              <tr>
+                <th scope="col">Action</th>
+                <th scope="col">Date</th>
+                <th scope="col">Time</th>
+                <th scope="col">Success</th>
+              </tr>
+            </thead>
+            <tbody>{renderLog()}</tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
