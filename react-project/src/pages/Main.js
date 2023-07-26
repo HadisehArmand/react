a39e934b52d82ffe7a8c8d30fd04742ab51cc281
Hasdiseh <133518815+HadisehArmand@users.noreply.github.com>
2023-07-26 12:49:47 +0330

    change main.js

--- react-project/src/pages/Main.js
+++ react-project/src/pages/Main.js
@@ -1,102 +1,81 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import getUser from "../getUser";
 import Login from "./Login";
 
-class Main extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      islogin:  (JSON.parse(localStorage.getItem("islogin"))=== true),
-      loginpage : !(JSON.parse(localStorage.getItem("islogin"))=== true)
-    }
-    this.logout = this.logout.bind(this);
-  }
-  componentDidMount() {
+const Main = (props) => {
+  const [isLogin, setIsLogin] = useState(
+    JSON.parse(localStorage.getItem("islogin")) === true
+  );
+  const [loginPage, setLoginPage] = useState(
+    !(JSON.parse(localStorage.getItem("islogin")) === true)
+  );
+
+  const logout = () => {
+    localStorage.setItem("islogin", !isLogin);
+    setIsLogin(JSON.parse(localStorage.getItem("islogin")) === true);
+    setLoginPage(!(JSON.parse(localStorage.getItem("islogin")) === true));
+  };
+
+  useEffect(() => {
     getUser();
-  }
-  logout(){
-    localStorage.setItem('islogin',(!(this.state.islogin)))
-    this.setState({
-      islogin:  (JSON.parse(localStorage.getItem("islogin"))=== true),
-      loginpage : !(JSON.parse(localStorage.getItem("islogin"))=== true)
-    })
-  }
-  render() {
-    return (
-<div>
-  {
-this.state.islogin && (
-  <div className="body">
-  <div className="container ">
-    <div className="row">
-      <h1 className="header">User Managment</h1>
+  }, []);
+
+  return (
+    <div>
+      {isLogin && (
+        <div className="body">
+          <div className="container ">
+            <div className="row">
+              <h1 className="header">User Management</h1>
+            </div>
+            <div className="row">
+              <div className=" d-grid justify-content-center gap-2 ">
+                <Link className="disable-link" to="/view">
+                  <input
+                    className="btn-index header-btn-index btn-primary"
+                    type="button"
+                    defaultValue="view"
+                  />
+                </Link>
+              </div>
+            </div>
+            <div className="row">
+              <div className=" d-grid justify-content-center gap-2   ">
+                <Link className="disable-link" to="/add">
+                  <input
+                    className="btn-index header-btn-index btn-primary"
+                    type="button"
+                    defaultValue="Add/Edit"
+                  />
+                </Link>
+              </div>
+              <div className=" d-grid justify-content-center gap-2  ">
+                <Link className="disable-link" to="/log">
+                  <input
+                    className="btn-index header-btn-index btn-primary"
+                    type="button"
+                    defaultValue="Log"
+                    id="demo"
+                  />
+                </Link>
+              </div>
+              <div className=" d-grid justify-content-center gap-2  ">
+                <input
+                  className="btn-index header-btn-index btn-primary"
+                  type="button"
+                  defaultValue="Logout"
+                  onClick={logout}
+                  id="demo"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {loginPage && <Login />}
     </div>
-    <div className="row">
-      {/* <div className="col-6 d-flex justify-content-evenly">
-        <Link className="disable-link" to="/login">
-          <input
-            className="btn-index header-btn-index"
-            type="button"
-            defaultValue="Login"
-          />
-        </Link>
-      </div> */}
-      {/* <div class="d-grid gap-2 d-md-block">
-        <button class="btn btn-primary btn-out" type="button">
-          Log out
-        </button>
-      </div> */}
-      <div className=" d-grid justify-content-center gap-2 ">
-        <Link className="disable-link" to="/view">
-          <input
-            className="btn-index header-btn-index btn-primary"
-            type="button"
-            defaultValue="view"
-          />
-        </Link>
-      </div>
-    </div>
-    <div className="row">
-      <div className=" d-grid justify-content-center gap-2   ">
-        <Link className="disable-link" to="/add">
-          <input
-            className="btn-index header-btn-index btn-primary"
-            type="button"
-            defaultValue="Add/Edit"
-          />
-        </Link>
-      </div>
-      <div className=" d-grid justify-content-center gap-2  ">
-        <Link className="disable-link" to="/log">
-          <input
-            className="btn-index header-btn-index btn-primary"
-            type="button"
-            defaultValue="Log"
-            id="demo"
-          />
-        </Link>
-      </div>
-      <div className=" d-grid justify-content-center gap-2  ">
-          <input
-            className="btn-index header-btn-index btn-primary"
-            type="button"
-            defaultValue="Logout"
-            onClick={this.logout}
-            id="demo"
-          />
-      </div>
-    </div>
-  </div>
-</div>
-)
-  }
-  {
-this.state.loginpage && <Login></Login>
-  }
-</div>
-    );
-  }
-}
+  );
+};
 
 export default Main;
