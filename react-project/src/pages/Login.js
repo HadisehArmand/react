8249c89e6b461c45480d362749b0ad9ed1d847da
Hasdiseh <133518815+HadisehArmand@users.noreply.github.com>
2023-07-26 12:34:39 +0330

    change login.js

--- react-project/src/pages/Login.js
+++ react-project/src/pages/Login.js
@@ -1,123 +1,97 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Main from "./Main";
 import add_admin from "../refresh";
 import getUser from "../getUser";
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    add_admin();
-    getUser();
-    this.state = {
-      email: "",
-      password: "",
-      main:((JSON.parse(localStorage.getItem("islogin"))=== true)),
-      admin_username: localStorage.getItem("admin-username"),
-      admin_password: localStorage.getItem("admin-password"),
-      islogin: !(JSON.parse(localStorage.getItem("islogin"))=== true),
-    };
-    this.retrievedObjectLG = JSON.parse(localStorage.getItem("users"));
-    this.checkuser = this.checkuser.bind(this);
 
-  }
-  checkuser() {
-    if (this.state.email !== "" && this.state.password !== "") {
-      if (
-        this.state.email === this.state.admin_username &&
-        this.state.password === this.state.admin_password
-      ) {
-        alert("login success !")
+const Login = () => {
+  add_admin();
+  getUser();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [main, setMain] = useState(
+    JSON.parse(localStorage.getItem("islogin")) === true
+  );
+  const admin_username = localStorage.getItem("admin-username");
+  const admin_password = localStorage.getItem("admin-password");
+  const [isLogin, setIsLogin] = useState(
+    !(JSON.parse(localStorage.getItem("islogin")) === true)
+  );
+
+  const retrievedObjectLG = JSON.parse(localStorage.getItem("users"));
+
+  useEffect(() => {
+    setIsLogin(JSON.parse(localStorage.getItem("islogin")) === true);
+    setMain(JSON.parse(localStorage.getItem("islogin")) === true);
+  }, []);
+
+  const checkuser = () => {
+    if (email !== "" && password !== "") {
+      if (email === admin_username && password === admin_password) {
+        alert("login success !");
         getUser();
-        this.setState({ islogin: false , main:true});
+        setIsLogin(false);
+        setMain(true);
         localStorage.setItem("islogin", true);
       }
     } else {
       alert("complete field");
     }
-  }
-  updateInputValue(evt) {
-    const val = evt.target.value;
-    const idV = evt.target.id;
-    this.setState({
-      [idV]: val,
-    });
-  }
-  //   checkuser() {
-  //     for (let index = 0; index < this.retrievedObjectLG.length; index++) {
-  //       if (
-  //         this.retrievedObjectLG[index]["email"] === this.state.email &&
-  //         this.retrievedObjectLG[index]["password"] === this.state.password
-  //       ) {
-  //         alert("User Login Success !");
-  //         SetAction("login with id " + this.state.id, "ok");
-  //         window.location.href = "/view";
-  //         break;
-  //       } else {
-  //         alert("User Login not success !");
-  //         SetAction("login with id " + this.state.id, "False");
-  //         break;
-  //       }
-  //     }
-  //   }
-  render() {
-    return (
-      <div>
-      {
-        (this.state.islogin) &&    
-           <div className="container-fluid d-flex justify-content-center mt-5">
-        <div className="row main-login">
-          <div className="row d-flex justify-content-center">
-            <h1 className="text-center">Login</h1>
-            <div className="input-group mb-3  d-flex justify-content-center">
-              <span className="input-group-text" id="basic-addon1">
-                @
-              </span>
-              <input
-                id="email"
-                type="email"
-                className="form-control"
-                placeholder="email"
-                aria-label="email"
-                aria-describedby="basic-addon1"
-                onChange={(evt) => this.updateInputValue(evt)}
-              />
+  };
+
+  return (
+    <div>
+      {isLogin && (
+        <div className="container-fluid d-flex justify-content-center mt-5">
+          <div className="row main-login">
+            <div className="row d-flex justify-content-center">
+              <h1 className="text-center">Login</h1>
+              <div className="input-group mb-3  d-flex justify-content-center">
+                <span className="input-group-text" id="basic-addon1">
+                  @
+                </span>
+                <input
+                  id="email"
+                  type="email"
+                  className="form-control"
+                  placeholder="email"
+                  aria-label="email"
+                  aria-describedby="basic-addon1"
+                  onChange={(evt) => setEmail(evt.target.value)}
+                />
+              </div>
             </div>
-          </div>
-          <div className="row d-flex justify-content-center">
-            <div className="input-group mb-3">
-              <span className="input-group-text" id="basic-addon1">
-                P
-              </span>
-              <input
-                id="password"
-                type="password"
-                className="form-control"
-                placeholder="password"
-                aria-label="password"
-                aria-describedby="basic-addon1"
-                onChange={(evt) => this.updateInputValue(evt)}
-              />
+            <div className="row d-flex justify-content-center">
+              <div className="input-group mb-3">
+                <span className="input-group-text" id="basic-addon1">
+                  P
+                </span>
+                <input
+                  id="password"
+                  type="password"
+                  className="form-control"
+                  placeholder="password"
+                  aria-label="password"
+                  aria-describedby="basic-addon1"
+                  onChange={(evt) => setPassword(evt.target.value)}
+                />
+              </div>
             </div>
-          </div>
-          <div className="row d-flex justify-content-center">
-            <div className="col-6  d-flex justify-content-evenly">
-              <input
-                className="btn-bt"
-                type="button"
-                value="Login"
-                onClick={this.checkuser}
-              />
+            <div className="row d-flex justify-content-center">
+              <div className="col-6  d-flex justify-content-evenly">
+                <input
+                  className="btn-bt"
+                  type="button"
+                  value="Login"
+                  onClick={checkuser}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      }
-      {
-           (this.state.main) && <Main/>
-      }
-      </div>
-
-    );
-  }
-}
+      )}
+      {main && <Main />}
+    </div>
+  );
+};
 
 export default Login;
