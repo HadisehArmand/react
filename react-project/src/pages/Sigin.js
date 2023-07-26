0d4a49b27ace1d90bbc7a8e30923986e4358bdc5
Hasdiseh <133518815+HadisehArmand@users.noreply.github.com>
2023-07-26 12:54:59 +0330

    change sign.js

--- react-project/src/pages/Sigin.js
+++ react-project/src/pages/Sigin.js
@@ -1,133 +1,191 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import SetAction from "../addLogsys";
 import getUser from "../getUser";
-class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.reset();
-        this.submitUser = this.submitUser.bind(this)
-    }
+const Login = (props) => {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [avatar, setAvatar] = useState("");
 
-    reset() {
-        this.setState({
-            firstname: '',
-            lastname: '',
-            email: '',
-            password: '',
-            avatar: '',
+  const reset = () => {
+    setFirstname("");
+    setLastname("");
+    setEmail("");
+    setPassword("");
+    setAvatar("");
+  };
+
+  const submitUser = () => {
+    if (
+      firstname !== "" &&
+      lastname !== "" &&
+      email !== "" &&
+      password !== "" &&
+      avatar !== ""
+    ) {
+      const user = {
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        password: password,
+        avatar: avatar,
+      };
+
+      axios
+        .post(
+          `https://64b53279f3dbab5a95c6e9e2.mockapi.io/api/v1/user/`,
+          user,
+          {
+            headers: {
+              "content-type": "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          alert(JSON.stringify(res.data) + "\nCreated !");
+          SetAction(`"Signin with id ${res.data.id}"`, "ok");
+          getUser();
+          window.location.href = "/view";
         })
-    }
-    submitUser() {
-        if (this.state.firstname !== '' && this.state.lastname !== '' && this.state.email !== '' && this.state.password !== '' && this.state.avatar !== '') {
-            let bodyUser = JSON.stringify(this.state);
-            axios.post(`https://64b53279f3dbab5a95c6e9e2.mockapi.io/api/v1/user/`,bodyUser, {
-                headers: {
-                    'content-type': 'application/json',
-                },
-            })
-            .then(res => {
-                alert(JSON.stringify(res.data) + "\nCreated !")
-                SetAction(`"Signin with id ${res.data.id}"`,"ok")
-                getUser()
-                window.location.href = "/view"
-            })
-        }else{
-            alert("plaese complite form !")
-            SetAction("Signin","False")
-        }
-    }
-    updateInputValue(evt) {
-        const val = evt.target.value;
-        const idV = evt.target.id;    
-        this.setState({
-            [idV]: val
+        .catch((error) => {
+          alert("An error occurred while creating the user.");
+          SetAction("Signin", "False");
         });
+    } else {
+      alert("Please complete the form !");
+      SetAction("Signin", "False");
     }
-    render() {
-        return (
-            <div className="container-fluid d-flex justify-content-center mt-5">
-                <div className="row main-sign-log">
-                    <div className="row d-flex justify-content-center">
-                        <h1 className="text-center">Sign Up</h1>
-                        <div className="input-group mb-3  d-flex justify-content-center">
-                            <span className="input-group-text" id="basic-addon1">F</span>
-                            <input
-                                id="firstname"
-                                type="text"
-                                className="form-control"
-                                placeholder="FirstName"
-                                aria-label="FirstName"
-                                aria-describedby="basic-addon1"
-                                onChange={evt => this.updateInputValue(evt)}
-                            />
-                        </div>
-                    </div>
-                    <div className="row d-flex justify-content-center">
-                        <div className="input-group mb-3">
-                            <span className="input-group-text" id="basic-addon1">L</span>
-                            <input
-                                id="lastname"
-                                type="text"
-                                className="form-control"
-                                placeholder="LastName"
-                                aria-label="LastName"
-                                aria-describedby="basic-addon1"
-                                onChange={evt => this.updateInputValue(evt)}
-                            />
-                        </div>
-                    </div>
-                    <div className="row d-flex justify-content-center">
-                        <div className="input-group mb-3">
-                            <span className="input-group-text" id="basic-addon1">@</span>
-                            <input
-                                id="email"
-                                type="email"
-                                className="form-control"
-                                placeholder="email"
-                                aria-label="email"
-                                aria-describedby="basic-addon1"
-                                onChange={evt => this.updateInputValue(evt)}
-                            />
-                        </div>
-                    </div>
-                    <div className="row d-flex justify-content-center">
-                        <div className="input-group mb-3">
-                            <span className="input-group-text" id="basic-addon1">P</span>
-                            <input
-                                id="password"
-                                type="password"
-                                className="form-control"
-                                placeholder="password"
-                                aria-label="password"
-                                aria-describedby="basic-addon1"
-                                onChange={evt => this.updateInputValue(evt)}
-                            />
-                        </div>
-                    </div>
-                    <div className="row d-flex justify-content-center">
-                        <div className="input-group mb-3">
-                            <span className="input-group-text" id="basic-addon1">A</span>
-                            <input
-                                id="avatar"
-                                type="text"
-                                className="form-control"
-                                placeholder="Avatar"
-                                aria-label="Avatar"
-                                aria-describedby="basic-addon1"
-                                onChange={evt => this.updateInputValue(evt)}
-                            />
-                        </div>
-                    </div>
-                    <div className="row d-flex justify-content-center">
-                        <div className="col-6  d-flex justify-content-evenly">
-                            <input className="btn" type="button" value="SignUp" onClick={this.submitUser} />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
+  };
+
+  const updateInputValue = (evt) => {
+    const val = evt.target.value;
+    const idV = evt.target.id;
+
+    switch (idV) {
+      case "firstname":
+        setFirstname(val);
+        break;
+      case "lastname":
+        setLastname(val);
+        break;
+      case "email":
+        setEmail(val);
+        break;
+      case "password":
+        setPassword(val);
+        break;
+      case "avatar":
+        setAvatar(val);
+        break;
+      default:
+        break;
     }
-}
+  };
+
+  return (
+    <div className="container-fluid d-flex justify-content-center mt-5">
+      <div className="row main-sign-log">
+        <div className="row d-flex justify-content-center">
+          <h1 className="text-center">Sign Up</h1>
+          <div className="input-group mb-3  d-flex justify-content-center">
+            <span className="input-group-text" id="basic-addon1">
+              F
+            </span>
+            <input
+              id="firstname"
+              type="text"
+              className="form-control"
+              placeholder="FirstName"
+              aria-label="FirstName"
+              aria-describedby="basic-addon1"
+              onChange={(evt) => updateInputValue(evt)}
+              value={firstname}
+            />
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">
+              L
+            </span>
+            <input
+              id="lastname"
+              type="text"
+              className="form-control"
+              placeholder="LastName"
+              aria-label="LastName"
+              aria-describedby="basic-addon1"
+              onChange={(evt) => updateInputValue(evt)}
+              value={lastname}
+            />
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">
+              @
+            </span>
+            <input
+              id="email"
+              type="email"
+              className="form-control"
+              placeholder="email"
+              aria-label="email"
+              aria-describedby="basic-addon1"
+              onChange={(evt) => updateInputValue(evt)}
+              value={email}
+            />
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">
+              P
+            </span>
+            <input
+              id="password"
+              type="password"
+              className="form-control"
+              placeholder="password"
+              aria-label="password"
+              aria-describedby="basic-addon1"
+              onChange={(evt) => updateInputValue(evt)}
+              value={password}
+            />
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">
+              A
+            </span>
+            <input
+              id="avatar"
+              type="text"
+              className="form-control"
+              placeholder="Avatar"
+              aria-label="Avatar"
+              aria-describedby="basic-addon1"
+              onChange={(evt) => updateInputValue(evt)}
+              value={avatar}
+            />
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <div className="col-6  d-flex justify-content-evenly">
+            <input
+              className="btn"
+              type="button"
+              value="SignUp"
+              onClick={submitUser}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
